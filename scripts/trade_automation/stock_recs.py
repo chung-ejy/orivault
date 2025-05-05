89b32387e7b595e6e1a28445fb78f5fcaf3939b5
chung-ejy <chung.ejy@gmail.com>
2025-05-05 09:31:30 -0400

    weekly only

--- scripts/trade_automation/stock_recs.py
+++ scripts/trade_automation/stock_recs.py
@@ -19,51 +19,52 @@ orivault = ADatabase("ori")
 market = ADatabase("market")
 alp = AlpacaExtractor(paper=paper)
 
-orivault.cloud_connect()
-a = orivault.retrieve("results")
-orivault.disconnect()
-valid = a[(a["position_type"]=="long") & (a["timeframe"]=="week") & (a["selection_type"]!="long_short")].sort_values("pnl",ascending=False).head(20)
-top = valid.head(1).to_dict(orient="records")[0]
-pm = Portfolio(timeframe=top["timeframe"], ranking_metric=top["ranking_metric"], position_type=top["position_type"], grouping_type=top["grouping_type"].lower(), selection_type=top["selection_type"], allocation_type=top["allocation_type"], risk_type=top["risk_type"], selection_percentage=top["selection_percentage"])
+if datetime.now().weekday() == 0: # Monday
+    orivault.cloud_connect()
+    a = orivault.retrieve("results")
+    orivault.disconnect()
+    valid = a[(a["position_type"]=="long") & (a["timeframe"]=="week") & (a["selection_type"]!="long_short")].sort_values("pnl",ascending=False).head(20)
+    top = valid.head(1).to_dict(orient="records")[0]
+    pm = Portfolio(timeframe=top["timeframe"], ranking_metric=top["ranking_metric"], position_type=top["position_type"], grouping_type=top["grouping_type"].lower(), selection_type=top["selection_type"], allocation_type=top["allocation_type"], risk_type=top["risk_type"], selection_percentage=top["selection_percentage"])
 
-market.cloud_connect()
-index = market.retrieve("ticker_overview")
-market.disconnect()
-sic_codes = pd.read_csv("./csv_files/sic_codes.csv").rename(columns={"SIC Code":"sic_code","Office":"office"}).dropna()
+    market.cloud_connect()
+    index = market.retrieve("ticker_overview")
+    market.disconnect()
+    sic_codes = pd.read_csv("./csv_files/sic_codes.csv").rename(columns={"SIC Code":"sic_code","Office":"office"}).dropna()
 
-index["sic_code"] = index["sic_code"].fillna(0).astype(int)
-sic_codes["sic_code"] = sic_codes["sic_code"].fillna(0).astype(int)
-index = index.merge(sic_codes[["sic_code","office"]],on="sic_code")
+    index["sic_code"] = index["sic_code"].fillna(0).astype(int)
+    sic_codes["sic_code"] = sic_codes["sic_code"].fillna(0).astype(int)
+    index = index.merge(sic_codes[["sic_code","office"]],on="sic_code")
 
-end = alp.clock()["date"] - timedelta(days=1)
-start = (end - timedelta(days=200))
+    end = alp.clock()["date"] - timedelta(days=1)
+    start = (end - timedelta(days=200))
 
-prices = []
-for ticker in index["ticker"]: 
-    try:
-        price = alp.prices(ticker,start,end)
-        price = p.lower_column(price)
-        price = p.utc_date(price)
-    
-        price.sort_values("date", inplace=True)
-        price = p.additional_date_columns(price)
-        for member in Indicator:
-            price = member.calculate(price,live=True)
-        for member in RiskType:
-            price = member.apply(price)
-        prices.append(price)
-        sleep(0.35)
-    except Exception as e:
-        print(str(e))
-        continue
+    prices = []
+    for ticker in index["ticker"]: 
+        try:
+            price = alp.prices(ticker,start,end)
+            price = p.lower_column(price)
+            price = p.utc_date(price)
+        
+            price.sort_values("date", inplace=True)
+            price = p.additional_date_columns(price)
+            for member in Indicator:
+                price = member.calculate(price,live=True)
+            for member in RiskType:
+                price = member.apply(price)
+            prices.append(price)
+            sleep(0.35)
+        except Exception as e:
+            print(str(e))
+            continue
 
-simulation = pd.concat(prices).merge(index[["ticker","name","market_cap","office","sic_description"]],on="ticker",how="left")
-simulation["market_cap"] = simulation["market_cap"].astype(float)
-simulation["asset_class"] = "common_stock"
-simulation.sort_values("date", inplace=True)
-trades = pm.recs(simulation.copy())
+    simulation = pd.concat(prices).merge(index[["ticker","name","market_cap","office","sic_description"]],on="ticker",how="left")
+    simulation["market_cap"] = simulation["market_cap"].astype(float)
+    simulation["asset_class"] = "common_stock"
+    simulation.sort_values("date", inplace=True)
+    trades = pm.recs(simulation.copy())
 
-orivault.cloud_connect()
-orivault.drop("recommendations")
-orivault.store("recommendations",trades)
-orivault.disconnect()
+    orivault.cloud_connect()
+    orivault.drop("recommendations")
+    orivault.store("recommendations",trades)
+    orivault.disconnect()
